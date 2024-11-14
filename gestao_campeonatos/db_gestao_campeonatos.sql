6ff7e02e8c3d1175485f7e6e57629b779c9782f3
João Pedro Pinto Silva <105960437+JPPS26@users.noreply.github.com>
2024-11-14 13:28:35 +0000

    Update db_gestao_campeonatos.sql
    Nova base de dados

--- gestao_campeonatos/db_gestao_campeonatos.sql
+++ gestao_campeonatos/db_gestao_campeonatos.sql
@@ -1,34 +1,37 @@
--- Criação do banco de dados
+-- Criação da base de dados
 CREATE DATABASE gestao_campeonatos;
 
--- Selecionar o banco de dados
+-- Selecionar a base de dados
 USE gestao_campeonatos;
 
--- Tabela de usuários
+-- Criação da tabela utilizadores
 CREATE TABLE utilizadores (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_utilizador INT AUTO_INCREMENT PRIMARY KEY,
     nome_completo VARCHAR(100) NOT NULL,
     id_jogador VARCHAR(50) UNIQUE NOT NULL,
     data_nascimento DATE NOT NULL,
     numero_documento_identificacao VARCHAR(50) NOT NULL,
-    tipo_utilizador ENUM('administrador', 'utilizador') NOT NULL,
+    tipo_utilizador ENUM('administrador', 'utilizador') NOT NULL DEFAULT 'utilizador',
     palavra_passe VARCHAR(255) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Tabela de campeonatos
+-- Criação da tabela campeonatos
 CREATE TABLE campeonatos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    id_campeonato INT AUTO_INCREMENT PRIMARY KEY,
+    nome_campeonato VARCHAR(100) NOT NULL,
     data_inicio DATE NOT NULL,
-    data_fim DATE NOT NULL
-);
+    data_fim DATE NOT NULL,
+    CONSTRAINT chk_data CHECK (data_fim > data_inicio)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Tabela de pontuações
-CREATE TABLE pontuacoes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    id_jogador VARCHAR(50) NOT NULL,
-    campeonato_id INT NOT NULL,
-    pontuacao INT NOT NULL,
-    FOREIGN KEY (id_jogador) REFERENCES usuarios(id_jogador),
-    FOREIGN KEY (campeonato_id) REFERENCES campeonatos(id)
-);
+-- Criação da tabela Temporadas
+CREATE TABLE temporadas (
+    id_temporada INT AUTO_INCREMENT PRIMARY KEY,
+    ano_temporada INT NOT NULL CHECK (ano_temporada >= 1900 AND ano_temporada <= YEAR(CURDATE())),
+    numero_jogos INT NOT NULL,
+    pontuacoes INT NOT NULL,
+    id_campeonato INT,
+    id_utilizador INT NULL,
+    FOREIGN KEY (id_campeonato) REFERENCES campeonatos(id_campeonato) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_utilizador) REFERENCES utilizadores(id_utilizador) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
