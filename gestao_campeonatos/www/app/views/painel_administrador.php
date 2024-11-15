abbe415ab417c9b60f67d3513e8e91107dfc3c9d
João Pedro Pinto Silva <jpps262005@gmail.com>
2024-11-15 14:18:27 +0000

    favicon

--- gestao_campeonatos/www/app/views/painel_administrador.php
+++ gestao_campeonatos/www/app/views/painel_administrador.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['id_utilizador']) || $_SESSION['tipo_utilizador'] !== 'admi
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel do Administrador</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
