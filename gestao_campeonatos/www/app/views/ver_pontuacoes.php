0bdcb04122e2fe05801466b6745fb566b560b4f7
João Pedro Pinto Silva <jpps262005@gmail.com>
2024-11-15 12:28:35 +0000

    commit

--- gestao_campeonatos/www/app/views/ver_pontuacoes.php
+++ gestao_campeonatos/www/app/views/ver_pontuacoes.php
@@ -22,10 +22,10 @@ if (!isset($_SESSION['id_utilizador']) || $_SESSION['tipo_utilizador'] !== 'util
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="ver_pontuacoes.php">Ver Pontuações</a>
+                        <a class="nav-link" href="editar_perfil.php">Editar Perfil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="editar_perfil.php">Editar Perfil</a>
+                        <a class="nav-link" href="ver_pontuacoes.php">Ver Pontuações</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="ver_classificacoes.php">Ver Classificações</a>
